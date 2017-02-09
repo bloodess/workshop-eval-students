9477fb0b7136d2091105ec8763ed2c24b39ddca8
ArnaudC <ArnaudC@MacBook-Pro-de-imakumo-mac118.local>
2017-02-09 11:24:52 +0100

    Exercise 1

--- src/store.js
+++ src/store.js
@@ -38,7 +38,8 @@ function calculateTotalprice(products) {
   for (var i = products.length - 1; i >= 0; i--) {
     sum += products[i].price;
   }
-  return sum;
+  //return sum.toFixed(2);
+  return Math.round(sum*100)/100;
 }
 
 function toString(currency, rateCurrency) {
@@ -46,7 +47,8 @@ function toString(currency, rateCurrency) {
   for (var i = products.length - 1; i >= 0; i--) {
     result += '\n - ' + products[i].toString(currency, rateCurrency);
   }
-  return result;
+  console.log('type of : ------ > '+ typeof result);
+  return  (''+result);
 }
 
 function open() {
